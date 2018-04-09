4202ecbd9cae62d2153d5bc5003494cf03913888
Lior Rabin <lior@colu.com>
2018-04-09 09:44:46 +0300

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Osseus Config
 
-Osseus configuration module
+The Osseus configuration module parses: 
+
+* CLI arguments
+* Environment variables
+* Environment files
+
+The result is an object which will be passed to other Osseus modules
 
 ### Install
 ```bash
@@ -8,9 +14,30 @@ $ npm install osseus-config
 ```
 
 ### Usage
-```bash
-const OsseusConfig = require('osseus-config')
 
-const config = await OsseusLogger.init(config)
-const value = config.get("KEY_NAME")
+##### config/LOCAL.js
+```code
+module.exports = {
+  DEBUG: true,
+  OSSEUS_LOGGER_LOG_LEVEL: 'debug',
+  OSSEUS_SERVER_PORT: '8888'
+}
+```
+
+##### index.js
+```code
+const OsseusConfig = require('osseus-config')
+const config = await OsseusConfig.init()
+
+console.log(`env: ${config.env}`) // env: local
+console.log(`debug: ${config.debug}`) // debug: true
+console.log(`logger_log_level: ${config.logger.log_level}`) // logger_log_level: debug
+console.log(`server_port: ${config.server.port}`) // server_port: 8888
+
+console.log(`config keys: ${config.keys}`) // config keys: env,debug,logger,server
+```
+
+##### CLI
+```console
+$ node index.js --ENV LOCAL
 ```
--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 const path = require('path')
 const argv = require('yargs').argv
+const _ = require('lodash')
 
-let envFile = {}
+const parser = function (keys, data) {
+  const result = {}
+  keys.forEach((k) => {
+    let lowerK = k.toLowerCase()
+    if (_.startsWith(lowerK, 'osseus_')) {
+      let temp = lowerK.split('_')
+      let topKey = temp[1]
+      let innerKey = _.slice(temp, 2, temp.length).join('_')
+
+      result[topKey] = result[topKey] || {}
+      result[topKey][innerKey] = data[k].toLowerCase()
+    } else {
+      result[lowerK] = data[k].toLowerCase()
+    }
+  })
+  return result
+}
+
+const cliParser = function () {
+  const keys = _.difference(Object.keys(argv), ['_', 'help', 'version', '$0'])
+  return parser(keys, argv)
+}
+
+const envParser = function () {
+  // TODO
+  return {}
+}
+
+const fileParser = function () {
+  const cwd = process.cwd()
+  const env = argv['ENV'] || process.env['ENV']
+
+  if (!env) {
+    return {}
+  }
+
+  const envFile = require(path.join(cwd, '/config/', env))
+  const keys = Object.keys(envFile)
+  return parser(keys, envFile)
+}
 
 const init = function () {
-  var self = this
-  return new Promise((resolve, reject) => {
-    const cwd = process.cwd()
-    const env = argv['ENV'] || process.env['ENV']
+  let self = this
 
-    if (env) {
-      envFile = require(path.join(cwd, '/config/', env))
-      let keys = Object.keys(envFile)
-      keys.forEach((k) => {
-        process.env[k] = envFile[k]
-      })
-    }
+  return new Promise((resolve, reject) => {
+    const cliConf = cliParser()
+    const envConf = envParser()
+    const fileConf = fileParser()
+
+    self = _.assign({}, cliConf, envConf, fileConf)
+    self.keys = Object.keys(self)
 
     resolve(self)
   })
 }
 
-const get = function (param) {
-  param = param.toUpperCase()
-  return argv[param] || process.env[param] || envFile[param]
-}
-
 module.exports = {
-  init: init,
-  get: get
+  init: init
 }
