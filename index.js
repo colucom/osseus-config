9ba8d3ff884eb820713b9fb94353cf00cc59c6ab
Lior Rabin <lior@colu.com>
2018-09-27 15:13:26 +0300

    removed unused require

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const env = (argv['ENV'] || process.env['ENV'] || process.env['CFG_ENV'] || '').
 const application = (argv['APPLICATION_NAME'] || process.env['APPLICATION_NAME'] || process.env['CFG_APPLICATION_NAME'] || '').toUpperCase()
 const endpoint = argv['AWS_SECRETS_ENDPOINT'] || process.env['AWS_SECRETS_ENDPOINT'] || process.env['CFG_AWS_SECRETS_ENDPOINT'] || 'https://secretsmanager.eu-west-1.amazonaws.com'
 const region = argv['AWS_REGION'] || process.env['AWS_REGION'] || process.env['CFG_AWS_REGION'] || 'eu-west-1'
-const util = require('util')
 const aws = require('./lib/aws')()
 
 console.log(`env: ${env || 'undefined'}, application: ${application || 'undefined'}`)
